b8cd0852705081956dcd110f4b4a4c6b8cf38858
Henrique Furtado <63440926+HenriqueFurtado-Dev@users.noreply.github.com>
2022-09-28 18:11:10 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Typing anomation
+var typed = new Typed(".typing", {
+    strings:["", "Web Designer", "Web developer", "Graphic Desginer", "Youtuber"],
+    typeSpeed: 200,
+    BackSpeed: 60,
+    loop: true
+})
